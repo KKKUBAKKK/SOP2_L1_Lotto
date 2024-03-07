ba4c5fa502ed5d0174da62fcec8a6683065d7c88
jakubkindracki <01180792@pw.edu.pl>
2024-03-07 02:51:56 +0100

    Working stage 3. Need to complete stage 4 and maybe ass some signal handlders?

--- sop-lotto.c
+++ sop-lotto.c
@@ -28,56 +28,76 @@ void usage(char *name)
     exit(EXIT_FAILURE);
 }
 
-//char *get_guess_rand(int8_t lowest, int8_t highest)
-//{
-//    int8_t *res = calloc(GUESS_LENGTH, sizeof(int8_t));
-////    srand(getpid());
-//    for (int i = 0; i < GUESS_LENGTH; i++)
-//        res[i] = lowest + rand() % highest;
-//    return (char *) res;
-//}
-
-void player_work(int fdsIn, int fdsOut)
+void player_work(int fdsIn, int fdsOut, int T)
 {
-    int guess[GUESS_LENGTH];
-    srand(getpid());
-    draw(guess);
-    int luckyNumbers[GUESS_LENGTH];
+    int guess[GUESS_LENGTH], luckyNumbers[GUESS_LENGTH], matches, amount_spent = 0, win, amount_won = 0;
     pid_t userId = getpid();
+    srand(userId);
+    for (int i = 0; i < T; i++) {
+//        if (rand() % 9 == 0)
+//        {
+//            printf("%d: This is a waste of money\n", userId);
+//            break;
+//        }
+        amount_spent += 3;
+        draw(guess);
+        if (TEMP_FAILURE_RETRY(write(fdsOut, &userId, sizeof(pid_t))) < 0)
+            ERR("write");
+        if (TEMP_FAILURE_RETRY(write(fdsOut, guess, sizeof(guess))) < 0)
+            ERR("write");
+        if (TEMP_FAILURE_RETRY(read(fdsIn, luckyNumbers, sizeof(luckyNumbers))) < 1)
+            ERR("read");
+        matches = compare(guess, luckyNumbers);
+        win = get_reward(matches);
+        amount_won += win;
+        if (win > 0)
+            printf("%d: I won %d zl.\n", userId, win);
+    }
     if (TEMP_FAILURE_RETRY(write(fdsOut, &userId, sizeof(pid_t))) < 0)
         ERR("write");
-    if (TEMP_FAILURE_RETRY(write(fdsOut, guess, sizeof(guess))) < 0)
+    if (TEMP_FAILURE_RETRY(write(fdsOut, &amount_won, sizeof(int))) < 0)
         ERR("write");
-    if (TEMP_FAILURE_RETRY(read(fdsIn, luckyNumbers, sizeof(luckyNumbers))) < 1)
-        ERR("read");
-    printf("%d received lucky numbers: %d %d %d %d %d %d\n", userId, luckyNumbers[0], luckyNumbers[1],
-           luckyNumbers[2], luckyNumbers[3], luckyNumbers[4], luckyNumbers[5]);
+    if (TEMP_FAILURE_RETRY(write(fdsOut, &amount_spent, sizeof(int))) < 0)
+        ERR("write");
 }
 
-void totalizator_work(int n, int *fdsIn, int *fdsOut)
+void totalizator_work(int n, int *fdsIn, int *fdsOut, int T)
 {
-    char players_guesses[GUESS_LENGTH];
+    int players_guesses[GUESS_LENGTH], lucky_numbers[GUESS_LENGTH], amount_won, amount_spent;
     pid_t playerId;
-    int lucky_numbers[GUESS_LENGTH];
-    draw(lucky_numbers);
-
-    // Reading from players pipes their guesses
+    for (int t = 0; t < T; t++) {
+        draw(lucky_numbers);
+        for (int i = 0; i < n; i++) {
+            if (TEMP_FAILURE_RETRY(read(fdsIn[i], &playerId, sizeof(pid_t))) < 1)
+                ERR("read");
+            if (TEMP_FAILURE_RETRY(read(fdsIn[i], players_guesses, sizeof(players_guesses))) < 1)
+                ERR("read");
+            printf("Totalizator Sportowy: %d bet: %d %d %d %d %d %d\n", playerId, players_guesses[0],
+                   players_guesses[1], players_guesses[2], players_guesses[3], players_guesses[4], players_guesses[5]);
+            if (TEMP_FAILURE_RETRY(write(fdsOut[i], lucky_numbers, sizeof(lucky_numbers))) < 0)
+                ERR("write");
+        }
+        printf("Totalizator sportowy: %d %d %d %d %d %d are today's lucky numbers\n\n", lucky_numbers[0],
+               lucky_numbers[1], lucky_numbers[2], lucky_numbers[3], lucky_numbers[4], lucky_numbers[5]);
+    }
+    printf("\nFINAL STATISTICS:\n");
     for (int i = 0; i < n; i++)
     {
         if (TEMP_FAILURE_RETRY(read(fdsIn[i], &playerId, sizeof(pid_t))) < 1)
             ERR("read");
-        if (TEMP_FAILURE_RETRY(read(fdsIn[i], players_guesses, sizeof(players_guesses))) < 1)
+        if (TEMP_FAILURE_RETRY(read(fdsIn[i], &amount_won, sizeof(int))) < 1)
             ERR("read");
-        printf("Totalizator Sportowy: %d bet: %d %d %d %d %d %d\n", playerId, players_guesses[0],
-               players_guesses[1], players_guesses[2], players_guesses[3], players_guesses[4], players_guesses[5]);
-        if (TEMP_FAILURE_RETRY(write(fdsOut[i], lucky_numbers, sizeof(lucky_numbers))) < 0)
-            ERR("write");
+        if (TEMP_FAILURE_RETRY(read(fdsIn[i], &amount_spent, sizeof(int))) < 1)
+            ERR("read");
+        printf("Player %d: won %d and spent %d\n", playerId, amount_won, amount_spent);
+        if (TEMP_FAILURE_RETRY(close(fdsIn[i])))
+            ERR("close");
+        if (TEMP_FAILURE_RETRY(close(fdsOut[i])))
+            ERR("close");
     }
-    printf("Totalizator sportowy: %d %d %d %d %d %d are today's lucky numbers\n", lucky_numbers[0], lucky_numbers[1],
-           lucky_numbers[2], lucky_numbers[3], lucky_numbers[4], lucky_numbers[5]);
 }
 
-void create_players_and_pipes(int n, int *fdsIn, int *fdsOut)
+void create_players_and_pipes(int n, int *fdsIn, int *fdsOut, int T)
 {
     int tmpfdIn[2];
     int tmpfdOut[2];
@@ -104,7 +124,7 @@ void create_players_and_pipes(int n, int *fdsIn, int *fdsOut)
                 if (TEMP_FAILURE_RETRY(close(tmpfdOut[0])))
                     ERR("close");
                 printf("%d: I'm going to play Lotto\n", getpid());
-                player_work(tmpfdIn[0], tmpfdOut[1]);
+                player_work(tmpfdIn[0], tmpfdOut[1], T);
                 if (TEMP_FAILURE_RETRY(close(tmpfdIn[0])))
                     ERR("close");
                 if (TEMP_FAILURE_RETRY(close(tmpfdOut[1])))
@@ -124,40 +144,22 @@ void create_players_and_pipes(int n, int *fdsIn, int *fdsOut)
 
 int main(int argc, char **argv)
 {
-    pid_t pid = getpid();
-    srand(pid);
-
-    int bet[6], numbers[6];
-
-    for (int i = 0; i < 100; ++i) {
-        draw(bet);
-        draw(numbers);
-        int matches = compare(bet, numbers);
-        int reward = get_reward(matches);
-        if(reward == 0) continue;
-        printf("bet: %d, %d, %d, %d, %d, %d\n", bet[0], bet[1], bet[2], bet[3], bet[4], bet[5]);
-        printf("draw: %d, %d, %d, %d, %d, %d\n", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]);
-        printf("%d numbers matches\n", matches);
-        printf("%d payout\n", reward);
-    }
-
-//    usage(argv[0]);
-
-    // moj kod odtad
     if (argc < 3)
         usage(argv[0]);
     int N = atoi(argv[1]);
     int T = atoi(argv[2]);
+    if (N < 1 || T < 1)
+        usage(argv[0]);
     int *fdsIn, *fdsOut;
     if (NULL == (fdsIn = (int *)malloc(sizeof(int) * N)))
         ERR("malloc");
     if (NULL == (fdsOut = (int *)malloc(sizeof(int) * N)))
         ERR("malloc");
-    create_players_and_pipes(N, fdsIn, fdsOut);
-    totalizator_work(N, fdsIn, fdsOut);
+    create_players_and_pipes(N, fdsIn, fdsOut, T);
+    totalizator_work(N, fdsIn, fdsOut, T);
     free(fdsIn);
     free(fdsOut);
+    while (wait(NULL) != -1);
 
-    // jeszcze zamykanie wszystkich pipeow i wywaitowanie dzieci, moze byc z handlerem sigchld
     exit(EXIT_SUCCESS);
 }
